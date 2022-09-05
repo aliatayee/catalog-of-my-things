f949b1821b2e14f276e7830e8d28e42a19ef9e11
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-05 19:58:50 +0530

    Add all the methods in the Item class

--- models/item.rb
+++ models/item.rb
@@ -1,5 +1,5 @@
 class Item
-  attr_reader :genre
+  attr_reader :genre, :author, :source, :label
 
   attr_accessor :publish_date
 
@@ -7,10 +7,27 @@ class Item
     @id = id || Random.rand(1..1000)
     @publish_date = publish_date
     @archived = archived
+
+    @genre = @author = @source = @label = nil
   end
 
-  def genre=(genre)
+  def add_genre(genre)
     @genre = genre
+    genre.add_item(self) unless @genre.items.include?(self)
+  end
+
+  def add_author(author)
+    @author = author
+    author.add_item(self) unless @author.items.include?(self)
+  end
+
+  def add_source(source)
+    @source = source
+    source.add_item(self) unless @source.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
     genre.add_item(self) unless @genre.items.include?(self)
   end
 end
