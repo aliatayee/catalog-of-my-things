f0bef8f2e3807aa6ac2b6fb2e17c33160c15dafe
aliatayee <aliaqaatayee@gmail.com>
2022-09-07 01:31:05 +0430

    Fix linter errors

--- controllers/book.rb
+++ controllers/book.rb
@@ -25,7 +25,7 @@ class BookController
     publisher = gets.chomp
     print 'How is the state of book cover: '
     cover_state = gets.chomp
-    new_book = Book.new(publisher,cover_state,publish_date)
+    new_book = Book.new(publisher, cover_state, publish_date)
     new_book.can_be_archived?
     @books << new_book
     puts 'Book added Successfully!'
@@ -33,16 +33,19 @@ class BookController
 
   def list_all_books
     return puts 'No Book listed yet' if @books.count.zero?
+
     puts 'Book List:'
     @books.each.with_index(1) do |book, i|
       print "\n#{i}) Publish Date: #{book.publish_date}, Cover State: #{book.cover_state}, "
       puts "Publish Date: #{book.publish_date}"
+    end
   end
-end
+
   def save_books
     book_object = @books.map do |book|
-        { id: book.id, publisher: book.publisher, cover_state: book.cover_state, publish_date: book.publish_date, archived: book.archived }
-      end
+      { id: book.id, publisher: book.publisher, cover_state: book.cover_state, publish_date: book.publish_date,
+        archived: book.archived }
+    end
     HandleFiles.write(@file_name, book_object)
   end
 
--- models/book.rb
+++ models/book.rb
@@ -4,7 +4,7 @@ class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state, publish_date, id: nil, archived: false)
-    super(id, publish_date, archived)
+    super(id, publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
   end
