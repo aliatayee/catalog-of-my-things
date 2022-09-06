fc7e30d3118568bc23fb4bcf8e7f1b61c51291da
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-06 15:45:46 +0530

    Implement static data list module based

--- modules/static_data.rb
+++ modules/static_data.rb
@@ -1,5 +1,17 @@
 module StaticData
-  def self.source_data
+  def self.sources
     ['From a friend', 'Online shop']
+  end
+
+  def self.genres
+    %w[Comedy Thriller]
+  end
+
+  def self.labels
+    %w[Gift New]
+  end
+
+  def self.authors
+    ['Stephen King', 'J K Rowling']
   end
 end
