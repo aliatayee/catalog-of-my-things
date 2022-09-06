715f53596f6e1c4a005ccb691de90518f9800a25
aliatayee <aliaqaatayee@gmail.com>
2022-09-06 10:19:49 +0430

    Update book class

--- models/book.rb
+++ models/book.rb
@@ -1,11 +1,14 @@
-class Book
-    attr_accessor :publisher, :cover_state
-    def initialize(publisher,cover_state)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+require './item'
+class Book < Item
+  attr_accessor :publisher, :cover_state, :publish_date, :archived
 
-    def can_be_archived?
-        super || cover_state == 'bad'
-    end
+  def initialize(publisher, cover_state, publish_date, id: nil, archived: false)
+    super(id: id, publish_date: publish_date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
 end
