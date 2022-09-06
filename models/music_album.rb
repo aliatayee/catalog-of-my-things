1a920154c720192fa48924329bd651bd845b53ba
bigmosi <kinyeramo@gmail.com>
2022-09-06 08:40:49 +0300

    work on music album

---
+++ models/music_album.rb
@@ -0,0 +1,14 @@
+require './item'
+
+class MusicAlbum < item
+  attr_accessor :on_spotify
+
+  def initialize(publish_date, archived, on_spotify: false)
+    super(publish_date, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    @on_spotify && Date.today.year - Date.parse(@publish_date).year > 10
+  end
+end
