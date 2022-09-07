d20977c54d890c2421709c3578f1545160c90664
Kinyera Amos <kinyeramo@gmail.com>
2022-09-07 12:54:18 +0300

    Revert "Preserved Data"

--- modules/static_data.rb
+++ modules/static_data.rb
@@ -1,12 +1,12 @@
 require_relative '../models/source'
-require_relative '../models/genre'
+
 module StaticData
   def self.sources
     ['From a friend', 'Online shop'].map { |source| Source.new(source) }
   end
 
   def self.genres
-    %w[Comedy Thriller].map { |genre| Genre.new(genre) }
+    %w[Comedy Thriller]
   end
 
   def self.labels
